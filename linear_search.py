bf2f7e84094219cbdf3958a7031f3dd702e13568
Samuel Bordenkecher <stb4380@gmail.com>
2025-10-25 20:11:22 -0400

    Solved

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,18 @@
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
-
+    x = value_to_find
+    arr = array_to_search_through
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+        else:
+            continue
 def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    x = value_to_find
+    arr = array_to_search_through
+    answer = []
+    for i in range(len(arr)):
+        if arr[i] == x:
+            answer.append(i)
+        else:
+            continue
+    return answer
